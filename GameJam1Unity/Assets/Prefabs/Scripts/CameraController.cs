6ba47f2e501de24e621583f6af6c269711b47839
JoshuaStrunk <joshua.p.strunk@gmail.com>
2014-11-08 03:22:19 -0600

    CameraController move/zoom

---
+++ GameJam1Unity/Assets/Prefabs/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public float smoothing = .5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow)) {
+			transform.position += Vector3.up * smoothing;
+		}
+		if(Input.GetKey(KeyCode.DownArrow)) {
+			transform.position -= Vector3.up* smoothing;
+		}
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right* smoothing;
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position -= Vector3.right* smoothing;
+		}
+		if(Input.GetMouseButton(3)) {
+			camera.orthographicSize += smoothing;
+		}
+		if(Input.GetMouseButton(4)) {
+			camera.orthographicSize -= smoothing;
+		}
+
+	}
+}
