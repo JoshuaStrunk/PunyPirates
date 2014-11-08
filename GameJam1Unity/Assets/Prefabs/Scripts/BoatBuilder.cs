107b2d5a3982c71e5d4a8ce086ea708113b53e48
JoshuaStrunk <joshua.p.strunk@gmail.com>
2014-11-08 03:09:36 -0600

    BoatBuilder.cs now Deluxe version

--- GameJam1Unity/Assets/Prefabs/Scripts/BoatBuilder.cs
+++ GameJam1Unity/Assets/Prefabs/Scripts/BoatBuilder.cs
@@ -3,26 +3,43 @@ using System.Collections;
 
 public class BoatBuilder : MonoBehaviour {
 
-
-	public int width = 4;
-	public int height = 2;
-
-	public float scale = 1f;
+	public int boatType = 0;
 	public GameObject boatChunk;
 
 	GameObject[,] boatChunks;
 
+	int width = 16;
+	int height = 6;
+	int boatsNum = 1;
+	int[,,] boats;
 
 	// Use this for initialization
 	void Start () {
+		//boatsNum, height, width
+		boats = new int[,,]{
+			{
+			{1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+			{1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+			}
+
+		};
+
+
 
 		boatChunks = new GameObject[width,height];
 
 		//Creation
 		for(int i = 0; i < width; i++) {
-			for(int j = 0; j<height; j++) {		
-				boatChunks[i,j] = (GameObject) Instantiate (boatChunk, transform.position + new Vector3(i,j,0), transform.rotation);
-				boatChunks[i,j].transform.parent = gameObject.transform;
+			for(int j = 0; j<height; j++) {
+
+				if(boats[boatType,height - j - 1,i] == 1) {
+					boatChunks[i,j] = (GameObject) Instantiate (boatChunk, transform.position + new Vector3(i,j,0), transform.rotation);
+					boatChunks[i,j].transform.parent = gameObject.transform;
+				}
 				
 
 			}
@@ -30,34 +47,38 @@ public class BoatBuilder : MonoBehaviour {
 		//Horizontal Connections
 		for(int i = 0; i<width-1; i++) {
 			for(int j=0; j<height; j++) {
-				HingeJoint2D tempHinge = boatChunks[i,j].AddComponent<HingeJoint2D>();
-				tempHinge.connectedBody = boatChunks[i+1,j].rigidbody2D;
-				tempHinge.anchor = new Vector2(.5f, 0f);
-				tempHinge.connectedAnchor = new Vector2(-.5f, 0f);
-				tempHinge.useLimits = true;
+				if((boats[boatType,height - j-1,i] == 1) && (boats[boatType,height -j-1,i+1] == 1)) {
+					HingeJoint2D tempHinge = boatChunks[i,j].AddComponent<HingeJoint2D>();
+					tempHinge.connectedBody = boatChunks[i+1,j].rigidbody2D;
+					tempHinge.anchor = new Vector2(.5f, 0f);
+					tempHinge.connectedAnchor = new Vector2(-.5f, 0f);
+					tempHinge.useLimits = true;
 
-				//Becuase C# is dumb
-				JointAngleLimits2D limits = tempHinge.limits;
-				limits.min = 0;
-				limits.max = 0;
-				tempHinge.limits = limits;
+					//Becuase C# is dumb
+					JointAngleLimits2D limits = tempHinge.limits;
+					limits.min = 0;
+					limits.max = 0;
+					tempHinge.limits = limits;
+				}
 			}
 		}
 
 		//Horizontal Connections
 		for(int i = 0; i<width; i++) {
 			for(int j=0; j<height-1; j++) {
-				HingeJoint2D tempHinge = boatChunks[i,j].AddComponent<HingeJoint2D>();
-				tempHinge.connectedBody = boatChunks[i,j+1].rigidbody2D;
-				tempHinge.anchor = new Vector2(0f, .5f);
-				tempHinge.connectedAnchor = new Vector2(0f, -.5f);
-				tempHinge.useLimits = true;
-				
-				//Becuase C# is dumb
-				JointAngleLimits2D limits = tempHinge.limits;
-				limits.min = 0;
-				limits.max = 0;
-				tempHinge.limits = limits;
+				if((boats[boatType,height-j-2,i] == 1) && (boats[boatType,height-j-1,i] == 1)) {
+					HingeJoint2D tempHinge = boatChunks[i,j].AddComponent<HingeJoint2D>();
+					tempHinge.connectedBody = boatChunks[i,j+1].rigidbody2D;
+					tempHinge.anchor = new Vector2(0f, .5f);
+					tempHinge.connectedAnchor = new Vector2(0f, -.5f);
+					tempHinge.useLimits = true;
+					
+					//Becuase C# is dumb
+					JointAngleLimits2D limits = tempHinge.limits;
+					limits.min = 0;
+					limits.max = 0;
+					tempHinge.limits = limits;
+					}
 			}
 		}
 		//Big O scary
@@ -66,7 +87,9 @@ public class BoatBuilder : MonoBehaviour {
 			for(int j = 0; j<height; j++) {
 				for(int ii = 0; ii <width; ii++) {
 					for(int jj=0; jj < height; jj++) {
-						Physics2D.IgnoreCollision(boatChunks[i,j].collider2D, boatChunks[ii,jj].collider2D, true);
+						if((boats[boatType,height - j - 1,i] == 1) && (boats[boatType,height - jj - 1,ii] == 1)) {
+							Physics2D.IgnoreCollision(boatChunks[i,j].collider2D, boatChunks[ii,jj].collider2D, true);
+						}
 					}
 				}
 			}
