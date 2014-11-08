4897f500fab801335483843522e80538c4bacf55
JoshuaStrunk <joshua.p.strunk@gmail.com>
2014-11-08 01:02:48 -0600

    Buoyancy and dumb boat builder

---
+++ GameJam1Unity/Assets/BoatBuilder.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoatBuilder : MonoBehaviour {
+
+
+	public int width = 5;
+	public int height = 2;
+	public GameObject boatChunk;
+
+	GameObject[,] boatChunks;
+
+
+	// Use this for initialization
+	void Start () {
+		boatChunks = new GameObject[5,2]; 
+		for(int i = 0; i < width; i++) {
+			for(int j = 0; j<height; j++) {
+				boatChunks[i,j] = (GameObject) Instantiate (boatChunk, transform.position + new Vector3(i,j,0), transform.rotation);
+				boatChunks[i,j].transform.parent = gameObject.transform;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ GameJam1Unity/Assets/Buoyancy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buoyancy : MonoBehaviour {
+
+
+	public float BuoantForce = 100f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter2D() {
+		rigidbody2D.velocity = Vector2.zero; //rigidbody.velocity * .1f;
+	}
+	void OnTriggerStay2D() {
+		Debug.Log("In Watter");
+		rigidbody2D.AddForce(Vector2.up * BuoantForce);
+	}
+}
