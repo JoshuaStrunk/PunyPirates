009444d8c83987ab25bf2ab836d40359125dd593
JoshuaStrunk <joshua.p.strunk@gmail.com>
2014-11-08 13:16:31 -0600

    CameraController Auto Focus

--- GameJam1Unity/Assets/Prefabs/Scripts/CameraController.cs
+++ GameJam1Unity/Assets/Prefabs/Scripts/CameraController.cs
@@ -4,14 +4,44 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public float smoothing = .5f;
+	public float minViewSize = 20f;
+	public float maxViewSize = 200f;
 
+	GameObject[] players;
 	// Use this for initialization
 	void Start () {
-	
+		players = GameObject.FindGameObjectsWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Vector3 averagePlayerPos = Vector3.zero;
+		foreach(GameObject player in players) {
+			averagePlayerPos += player.transform.position;
+		}
+		averagePlayerPos /= players.Length;
+
+		transform.position = new Vector3(averagePlayerPos.x, averagePlayerPos.y, transform.position.z);
+
+		float maxDiff = 0;
+
+		foreach(GameObject player in players) {
+			float diffOffAverage = (player.transform.position - averagePlayerPos).magnitude;
+			if(diffOffAverage > maxDiff ) {
+				maxDiff = diffOffAverage;
+			}
+		}
+		if(maxDiff < minViewSize) {
+			camera.orthographicSize = minViewSize;
+		}
+		else if(maxDiff > maxViewSize) {
+			camera.orthographicSize = maxViewSize;
+		}
+		else {
+			camera.orthographicSize = maxDiff;
+		}
+
+		/*
 		if(Input.GetKey(KeyCode.UpArrow)) {
 			transform.position += Vector3.up * smoothing;
 		}
@@ -30,6 +60,6 @@ public class CameraController : MonoBehaviour {
 		if(Input.GetMouseButton(4)) {
 			camera.orthographicSize -= smoothing;
 		}
-
+		*/
 	}
 }
