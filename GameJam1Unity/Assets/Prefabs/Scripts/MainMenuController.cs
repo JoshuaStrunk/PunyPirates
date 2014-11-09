e232a6d960961119f3e421a6d500a89921d55055
MarcosAlfonso <marcostocoast@gmail.com>
2014-11-09 12:20:32 -0600

    bug fix on main menu

--- GameJam1Unity/Assets/Prefabs/Scripts/MainMenuController.cs
+++ GameJam1Unity/Assets/Prefabs/Scripts/MainMenuController.cs
@@ -6,7 +6,7 @@ public class MainMenuController : MonoBehaviour {
 	void OnGUI() {
 		GUI.Label(new Rect(Screen.width/2-50, 0, 100, 50), "tiny PIRATES!!!");
 		if(GUI.Button(new Rect(Screen.width/2-50, Screen.height/2, 100, 30), "Start!")) {
-			Application.LoadLevel(1);
+			Application.LoadLevel(2);
 		}
 	}
 }
