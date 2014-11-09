eca4a12567712665f92cf078188787544c706c06
MarcosAlfonso <marcostocoast@gmail.com>
2014-11-09 03:09:23 -0600

    Graphical Stuff, Clouds, Color changing

---
+++ GameJam1Unity/Assets/Art/Shaders/GradientControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GradientControl : MonoBehaviour {
+
+	public Color startColor = Color.red;
+	public Color endColor = Color.blue;
+
+	// Use this for initialization	
+		void Start () {
+			Mesh mesh = GetComponent<MeshFilter>().mesh;
+			Color[] colors = new Color[mesh.vertices.Length];
+			colors[0] = startColor;
+			colors[1] = endColor;
+			colors[2] = startColor;
+			colors[3] = endColor;
+			mesh.colors = colors;
+		}
+	}
+	
---
+++ GameJam1Unity/Assets/CloudMover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudMover : MonoBehaviour {
+
+	public float moveRate = .1f;
+
+	// Use this for initialization
+	void Start () {
+		float randomHeight = Random.Range(10,40);
+
+		transform.position = new Vector3(transform.position.x, randomHeight, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position += new Vector3(moveRate, 0, 0);
+
+		if (transform.position.x > 180)
+		{
+			transform.position = new Vector3( -160, Random.Range(10,40), transform.position.z);
+		}
+	}
+}
